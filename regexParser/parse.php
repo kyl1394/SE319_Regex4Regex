7ae721bc57c537a7db6bfbf541c39e44618f2b24
georgethexiii <leos@iastate.edu>
2014-12-14 17:53:27 -0600

    UI

---
+++ regexParser/parse.php
@@ -0,0 +1,26 @@
+<?php
+
+    $file = fopen( "fileIO/statement.in", "w" );
+    fwrite( $file, $_GET['generator']);
+    fclose($file);
+    
+    $grun = "java org.antlr.v4.runtime.misc.TestRig ";
+
+
+    shell_exec($grun." regexParser start < fileIO/statement.in > fileIO/regex.out");
+
+    $file = fopen( "fileIO/regex.out", "r" );
+    $filesize = filesize( "fileIO/regex.out" );
+    $filetext = fread( $file, $filesize );
+    fclose($file);
+    
+    echo json_encode(
+        
+        array(
+            
+            'regex' => htmlspecialchars($filetext)
+        )
+    ); 
+
+
+?>
